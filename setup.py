a6419fa35299efad760a020fe5197772ad969662
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-11-15 15:21:38 +0100

    renamed QFitsView to qfitsview

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='qfitsview',
-    version='0.2',
+    version='0.3',
     description='PyQt widget for displaying FITS files',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
