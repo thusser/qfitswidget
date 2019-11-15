15d6187a429fddff09c376f3e0b98c44b38f4e10
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-11-15 15:13:52 +0100

    v0.2

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from distutils.core import setup
 
 setup(
     name='QFitsView',
-    version='0.1',
+    version='0.2',
     description='PyQt widget for displaying FITS files',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
-    packages=['qfitsview']
+    packages=['qfitsview'],
+    install_requires=[
+        'matplotlib',
+        'PyQt5',
+        'numpy',
+        'astropy'
+    ]
 )
